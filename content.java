ca3b7a1846195a00d7edf42a150e4c612f911e51
k-kahraman <knkhrmn.developer@gmail.com>
2021-07-13 12:09:00 +0000

    add code in files types: .json,.js,.java remove code in files types: .json,.js,.java

--- content.java
+++ content.java
@@ -1,6 +1,6 @@
 public class Cioiso {
     public static void main() {
         System.out.println("ecfus");
-        System.out.println("wehxa");
+        System.out.println("ortlt");
     }
 }
--- content.js
+++ content.js
@@ -1,2 +1,2 @@
 console.log("sedqk")
-console.log("gpadc")
+console.log("eswsg")
