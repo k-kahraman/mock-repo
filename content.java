ea3b256b5a65c829cbbdb8bc6f9dfd34796f6a2e
k-kahraman <knkhrmn.developer@gmail.com>
2021-07-16 11:57:44 +0000

    add code in files types: .js,.java remove code in files types: .js,.java

--- content.java
+++ content.java
@@ -1,6 +1,6 @@
 public class Cioiso {
     public static void main() {
         System.out.println("ecfus");
-        System.out.println("ixcuf");
+        System.out.println("ctwug");
     }
 }
--- content.js
+++ content.js
@@ -1,3 +1,3 @@
 console.log("lgvls")
 console.log("qtcqq")
-console.log("rhyad")
+console.log("evxrd")
