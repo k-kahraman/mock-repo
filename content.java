4240f8f70e06febabbc9f3c14cb233e36c86ea54
k-kahraman <knkhrmn.developer@gmail.com>
2021-09-10 11:59:37 +0000

    add code in files types: .json,.js,.mtl,.obj,.gltf,.svg,.css,.xml,.java,.properties remove code in files types: .json,.js,.css,.xml,.java,.properties

--- content.java
+++ content.java
@@ -5,6 +5,6 @@ public class Cioiso {
         System.out.println("xsjef");
         System.out.println("xrahv");
         System.out.println("tcfup");
-        System.out.println("colyv");
+        System.out.println("hunqi");
     }
 }
