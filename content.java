2a73902c1beea6489cb18b0e5ed8b2e5befc2aac
k-kahraman <knkhrmn.developer@gmail.com>
2021-09-21 16:17:43 +0000

    add code in files types: .xml,.java remove code in files types: .xml,.java

--- content.java
+++ content.java
@@ -11,6 +11,6 @@ public class Cioiso {
         System.out.println("jsfkx");
         System.out.println("rsibm");
         System.out.println("hjrgx");
-        System.out.println("vapwf");
+        System.out.println("shomm");
     }
 }
