95e4a4d86409c0b8a9ee5b795163b66fac8d6020
k-kahraman <knkhrmn.developer@gmail.com>
2021-10-22 16:51:05 +0000

    add code in files types: ,.pem,.conf,.js,.xml,.java,.properties,.py,.yml,.sh remove code in files types: .conf,.js,.xml,.java,.properties,.yml

--- content.java
+++ content.java
@@ -11,6 +11,6 @@ public class Cioiso {
         System.out.println("jsfkx");
         System.out.println("rsibm");
         System.out.println("hjrgx");
-        System.out.println("iehyw");
+        System.out.println("jvlkj");
     }
 }
--- content.py
+++ content.py
@@ -1,1 +1,2 @@
 print("farem")
+print("basmv")
