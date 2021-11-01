e34c6825a34e48844f3518645e1a79f520dfacb1
k-kahraman <knkhrmn.developer@gmail.com>
2021-11-01 13:00:24 +0000

    add code in files types: .json,.xml,.java remove code in files types: .json,.xml,.java

--- content.java
+++ content.java
@@ -12,6 +12,6 @@ public class Cioiso {
         System.out.println("rsibm");
         System.out.println("hjrgx");
         System.out.println("uejjg");
-        System.out.println("pkwyu");
+        System.out.println("mqvoo");
     }
 }
