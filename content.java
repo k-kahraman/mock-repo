bf75e2537d599ac2ba0b6726c28d2b83ac0c2391
k-kahraman <knkhrmn.developer@gmail.com>
2021-11-30 08:56:45 +0000

    add code in files types: .json,.java remove code in files types: .java,.sh

--- content.java
+++ content.java
@@ -13,6 +13,6 @@ public class Cioiso {
         System.out.println("hjrgx");
         System.out.println("uejjg");
         System.out.println("hnfvn");
-        System.out.println("wlnun");
+        System.out.println("iosdb");
     }
 }
