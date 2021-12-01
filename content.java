77e7f73de141b286cfbdbdcab3628ffcc335a1a8
k-kahraman <knkhrmn.developer@gmail.com>
2021-12-01 08:58:29 +0000

    add code in files types: .conf,.java remove code in files types: .java

--- content.java
+++ content.java
@@ -13,6 +13,6 @@ public class Cioiso {
         System.out.println("hjrgx");
         System.out.println("uejjg");
         System.out.println("hnfvn");
-        System.out.println("iosdb");
+        System.out.println("xsdcs");
     }
 }
